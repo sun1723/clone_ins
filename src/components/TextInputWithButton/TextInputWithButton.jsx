e8f0ad1e9c518b06892b2bafd4febd654ace6315
sun <sunpeiting07@gmail.com>
2022-09-28 23:32:34 +0930

    TA-1: create textInput component with button

---
+++ src/components/TextInputWithButton/TextInputWithButton.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react"
+import TextInput from "../TextInput"
+
+const TextInputWithButton = () =>{
+  return <Fragment>
+    <TextInput />
+    <button>button</button>
+  </Fragment>
+}
+
+export default TextInputWithButton;
