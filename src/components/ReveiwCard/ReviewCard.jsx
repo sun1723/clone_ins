d8bdab613bc102db22b89f2b10995324fb64eb08
sun <sunpeiting07@gmail.com>
2022-10-01 21:18:38 +0930

    TA-1: add review card

---
+++ src/components/ReveiwCard/ReviewCard.jsx
@@ -0,0 +1,15 @@
+const ReviewCard = props => {
+  const {review} = props;
+
+  return (
+    <div>
+      <img src={review.thumbNailImg} />
+      <span>{review.username}</span>
+      <span>{review.data}</span>
+      <span>{review.time}</span>
+      <p>{review.content}</p>
+    </div>
+  )
+}
+
+export default ReviewCard;
