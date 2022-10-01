ea5be9bd349e2755745a2357ea7267118b9b7e97
sun <sunpeiting07@gmail.com>
2022-10-01 21:15:57 +0930

    create mock response file for content display in landmarkList

---
+++ src/components/LandMarkList/__data__/mock-lanmark-list-response.js
@@ -0,0 +1,15 @@
+export const mockLandMarkListResponse = [
+  /**
+   * card:
+   *      name
+   *      location
+   */
+  {
+    name:'name 1',
+    location: 'xx st'
+  },
+  {
+    name: 'name 2',
+    location: 'pirie street'
+  }
+]
