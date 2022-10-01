a9062df7301e2c374a3990f489f7cd88babcc803
sun <sunpeiting07@gmail.com>
2022-10-01 21:15:24 +0930

    TA-1: add landmark list

---
+++ src/components/LandMarkList/LandMarkList.jsx
@@ -0,0 +1,14 @@
+import LandMarkCard from "../LandMarkCard";
+import List from "../List";
+import { mockLandMarkListResponse } from "./__data__/mock-lanmark-list-response";
+
+const LandMarkList = () => {
+  const child = mockLandMarkListResponse.map((item,i) =><LandMarkCard key={i} item={item} />)
+
+  const header = <div> Total 3 search results</div>
+  return (
+    <List>{header}{child}</List>
+  )
+}
+
+export default LandMarkList;
