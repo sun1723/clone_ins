fa98bfd0c9178d6c2c524de2ecfb037cdac8fc4f
sun <sunpeiting07@gmail.com>
2022-10-01 21:19:41 +0930

    TA-1: update app component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import LandMarkList from '../LandMarkList';
+import LandMarkView from '../LandMarkView';
+import ReviewList from '../ReviewList';
+import SearchBar from '../SearchBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <aside>
+        <SearchBar/>
+        <LandMarkList />
+      </aside>
+      <main>
+        <LandMarkView/>
+        <ReviewList />
+      </main>
       
     </div>
   );
