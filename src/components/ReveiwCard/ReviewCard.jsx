ebc26b0ee3d62c29bc28eebd4c0548d8a85bef01
sun <sunpeiting07@gmail.com>
2022-10-05 23:10:05 +1030

    ta-4: make images in review list smaller

--- src/components/ReveiwCard/ReviewCard.jsx
+++ src/components/ReveiwCard/ReviewCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import classes from './ReviewCard.scss';
+
 const ReviewCard = props => {
   const {review} = props;
 
   return (
     <div>
-      <img src={review.thumbNailImg} />
+      <img src={review.thumbNailImg} className={classes.reviewImg}/>
       <span>{review.username}</span>
       <span>{review.data}</span>
       <span>{review.time}</span>
